b813582e7a14465f9ba83696d47e969d9a31155b
Kiernan2 <kiernan1994@gmail.com>
2021-06-21 13:13:38 -0700

    Added day and night mode

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $("button#btnNight").click(function(){
     $("body").addClass("night")
   });
-  $("")
+  $("button#btnDay").click(function(){
+    $("body").removeClass("night")
+  });
 });
